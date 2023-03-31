f7bff2d283f69f10f994de3fb873c3cc23e46f44
Paul Gauthier <paul@geomagical.com>
2023-03-31 12:28:51 -0700

    update openai key

--- cloud-proxy/update_token.sh
+++ cloud-proxy/update_token.sh
@@ -4,4 +4,4 @@
 set -e
 
 gcloud config set project $PROJECT_ID
-echo -n $OPENAI_API_KEY | gcloud secrets create openai-api-key --data-file=-
+echo -n $OPENAI_API_KEY | gcloud secrets versions add openai-api-key --data-file=-
